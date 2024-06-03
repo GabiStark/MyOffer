6cff89c32969367740f771a4ebf58e7d2c62ba7b
GabiStark <gabriel.ferrom@gmail.com>
2024-06-03 21:58:23 +0200

    commit prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        title:"Home",
+        loadComponent: ()=> import("./layouts/home/home.component").then(m=>m.HomeComponent),
+        children: [
+          {
+            path: "topBar",
+            loadComponent: () => import ("./components/top-bar/top-bar.component").then(m => m.TopBarComponent),
+            title: "topBar"
+          },
+          {
+            path: "gridPrincipal",
+            loadComponent: () => import ("./components/grid-principal/grid-principal.component").then(m => m.GridPrincipalComponent),
+            title: "gridPrincipal"
+          },
+          {
+            path: "footer",
+            loadComponent: () => import ("./components/footer/footer.component").then(m => m.FooterComponent),
+            title: "footer"
+          }
+        ]
+    }
+
+];
